1f6ea12cf6c6fe6d4d21b64651030ba9bd386841
Lenny Nilsson <Lenny.Nilsson@betssongroup.com>
2015-12-02 14:09:50 +0100

    Added pivot value for when loosing line

--- lenny/lenny.ino
+++ lenny/lenny.ino
@@ -8,8 +8,11 @@ ZumoReflectanceSensorArray reflectanceSensors;
 ZumoMotors motors;
 Pushbutton button(ZUMO_BUTTON);
 int lastError = 0;
+unsigned int sensors[6];
 
 const int MAX_SPEED = 400;
+const int MAX_SENSOR = 1000;
+const int SENSOR_PIVOT = 500;
 
 void lightsOn() {
   digitalWrite(13, HIGH);
@@ -29,7 +32,6 @@ void setup() {
 }
 
 int getDirection() {
-  unsigned int sensors[6];
   int position = reflectanceSensors.readLine(sensors);
   int error = position - 2500;
   int speedDifference = error / 4 + 6 * (error - lastError);
@@ -37,8 +39,20 @@ int getDirection() {
   return speedDifference;
 }
 
+boolean onLine() {
+  int i;
+  boolean above = true;
+  for(i = 0; i < 6; i++) {
+    above &= (SENSOR_PIVOT < sensors[i])
+  }
+  return above;
+}
+
 void loop() {
   int lineDirection = getDirection();
+  if (onLine()) {
+    lineDirection = MAX_SPEED / 2;  
+  }
   int m1Speed = MAX_SPEED + lineDirection;
   int m2Speed = MAX_SPEED - lineDirection;
   if (m1Speed < 0)
