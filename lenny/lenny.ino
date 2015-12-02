00cc6742a3e1e6b340d84e4183237bcfaea88d83
Lenny Nilsson <Lenny.Nilsson@betssongroup.com>
2015-12-02 14:13:29 +0100

    Added calibration

--- lenny/lenny.ino
+++ lenny/lenny.ino
@@ -22,13 +22,29 @@ void lightsOff() {
   digitalWrite(13, LOW);
 }
 
+void calibrate() {
+  int i;
+  for(i = 0; i < 80; i++)
+  {
+    if ((i > 10 && i <= 30) || (i > 50 && i <= 70))
+      motors.setSpeeds(-200, 200);
+    else
+      motors.setSpeeds(200, -200);
+    reflectanceSensors.calibrate();
+
+    // Since our counter runs to 80, the total delay will be
+    // 80*20 = 1600 ms.
+    delay(20);
+  }
+  motors.setSpeeds(0,0);
+}
+
 void setup() {
   reflectanceSensors.init();
   button.waitForButton();
   pinMode(13, OUTPUT);
+  calibrate();
   lightsOn();
-  reflectanceSensors.calibrate();
-  motors.setSpeeds(0,0);
 }
 
 int getDirection() {
@@ -43,7 +59,7 @@ boolean onLine() {
   int i;
   boolean above = true;
   for(i = 0; i < 6; i++) {
-    above &= (SENSOR_PIVOT < sensors[i])
+    above &= (SENSOR_PIVOT < sensors[i]);
   }
   return above;
 }
